123d35421eee1fde80dcebc778aebb32a7931a37
SilviaAlarcon <silvia.alarcon.or@gmail.com>
2020-10-29 01:41:36 -0600

    Ejercicios de funciones

--- Funciones/ejerciciosResueltos/calculadora_test.py
+++ Funciones/ejerciciosResueltos/calculadora_test.py
@@ -1,3 +1,8 @@
+'''1.Hacer un módulo con las funciones básicas para una calculadora, 
+suma, resta, producto y división, en la función de división si el denomidador 
+es igual a cero debe de volver a leer el valor del denominador hasta que sea 
+distinto de cero (se recomienda utilizar el ciclo while y el condicional if)'''
+
 from calculadora import *
 
 print('La suma de los números es: ' + str(suma(10, 5)))
